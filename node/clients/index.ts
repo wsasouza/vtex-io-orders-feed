7d72672d3fc519a13fb7314fc8cf232f07bfd1e5
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-07-21 05:28:10 -0300

    Bug: Failed integration with Order Feed

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,9 +1,35 @@
-import { IOClients } from '@vtex/api';
-import { PointsClient } from './pointsClient';
+import type { ClientsConfig } from '@vtex/api'
+import { IOClients, LRUCache } from '@vtex/api';
 
-// Extend the default IOClients implementation with our own custom clients.
+import { Orders } from './orders'
+import { PointsClient } from './pointsClient';
 export class Clients extends IOClients {
-  public get points() {
+
+  public get orders() {
+    return this.getOrSet('orders', Orders)
+  }
+
+  public get pointsClient() {
     return this.getOrSet('points', PointsClient)
   }
 }
+
+const REQUESTS_TIMEOUT = 30000
+
+const memoryCache = new LRUCache<string, any>({ max: 5000 })
+
+metrics.trackCache('status', memoryCache)
+
+export const clients: ClientsConfig<Clients> = {
+  implementation: Clients,
+  options: {
+    default: {
+      retries: 2,
+      timeout: REQUESTS_TIMEOUT,
+    },
+    status: {
+      memoryCache,
+    },
+  },
+}
+
